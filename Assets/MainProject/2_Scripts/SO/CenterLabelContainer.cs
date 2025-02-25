6a21a2900ddef9d0e02bd48df0bb5d75b7d0381d
LSeungHyun <noblesse991015@gmail.com>
2025-02-25 18:54:44 +0900

    Add AllSheetContainers & Add TextDataManager CS

---
+++ Assets/MainProject/2_Scripts/SO/CenterLabelContainer.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CenterLabelContainer", menuName = "Scriptable Objects/CenterLabelContainer")]
+public class CenterLabelContainer : ScriptableObject
+{
+    public CenterLabelData[] centerLabelDatas;
+}
