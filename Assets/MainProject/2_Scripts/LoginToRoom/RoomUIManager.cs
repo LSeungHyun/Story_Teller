188729f130acb68db0c78f17d62c9045c2a0d5d9
LSeungHyun <noblesse991015@gmail.com>
2025-03-04 13:22:21 +0900

    Add Title_MV & Setting DontDestroy From Scene0 to Scene1

--- Assets/MainProject/2_Scripts/LoginToRoom/RoomUIManager.cs
+++ Assets/MainProject/2_Scripts/LoginToRoom/RoomUIManager.cs
@@ -6,11 +6,11 @@ public class RoomUIManager : MonoBehaviour
     [Header ("Canvase BackGround")]
     public GameObject bg_Blur;
     public GameObject bg_Start;
-    public void ActiveBlur(bool isBlur)
-    {
-        bg_Blur.SetActive(isBlur);
-        bg_Start.SetActive(!isBlur);
-    }
+    //public void ActiveBlur(bool isBlur)
+    //{
+    //    bg_Blur.SetActive(isBlur);
+    //    bg_Start.SetActive(!isBlur);
+    //}
     public void SceneMove(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
---
+++ Assets/MainProject/2_Scripts/LoginToRoom/TitleMP4Manager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TitleMP4Manager : MonoBehaviour
+{
+    public static TitleMP4Manager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
