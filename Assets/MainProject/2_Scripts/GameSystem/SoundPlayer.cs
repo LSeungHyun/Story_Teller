8a62f5a3ad83b1c0564703ad50c1191b1e4cd47a
LSeungHyun <noblesse991015@gmail.com>
2025-03-19 17:28:31 +0900

    setbound 테스트 및 제작 / 중단점

--- Assets/MainProject/2_Scripts/GameSystem/SoundPlayer.cs
+++ Assets/MainProject/2_Scripts/GameSystem/SoundPlayer.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class SoundPlayer : MonoBehaviour
 {
     public SoundContainer soundContainer;
+    public Collider2D camBound;
 
+        
     public string soundName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         soundContainer.soundManager.Play(soundName);
+        CamDontDes.instance.SetBound(camBound);
         this.gameObject.SetActive(false);
     }
 }
