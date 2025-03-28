3e0081d74951a9b10f596d876db2a5863074d3d7
yoonsung00 <ed2452950@naver.com>
2025-03-28 17:41:49 +0900

    인게임 UI 매니저 코드 작성 55% 완료

---
+++ Assets/MainProject/2_Scripts/1_WaitingRoom/UIManager.cs
@@ -0,0 +1,159 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class UIManager : DoTweenManager
+{
+    [System.Serializable]
+
+    public class PopUp_Group
+    {
+        public string PopUp_Name;
+        public GameObject PopUp_Obj;
+        public bool isAnim;
+    }
+
+    [System.Serializable]
+    public class Panel_Group
+    {
+        public string Panel_Name;
+        public GameObject Panel_Obj;
+        public bool isAnim;
+    }
+
+    [System.Serializable]
+    public class ChatStatus
+    {
+        public string Status;
+        public GameObject ShadowObj;
+        public GameObject IconObj;
+    }
+
+    [Header("PopUp List")]
+    [SerializeField]
+    private List<PopUp_Group> PopUp_List;
+
+    [Header("Panel List")]
+    [SerializeField]
+    private List<Panel_Group> Panel_List;
+
+    [Header("OnOffChat List")]
+    [SerializeField]
+    private List<ChatStatus> ChatStatusList;
+
+    [Header("Blur Object")]
+    [SerializeField]
+    private GameObject blurObject;
+
+    public Dictionary<string, PopUp_Group> popUpDict;
+    public Dictionary<string, Panel_Group> panelDict;
+
+    public bool blurAble = true;
+    public bool chatOn = false;
+
+    void Awake()
+    {
+        SetPopUpDict();
+        SetPanelDict();
+    }
+
+    void Start()
+    {
+
+    }
+
+    #region 딕셔너리 정보값 세팅 / 팝업, 패널
+
+    /// <summary>
+    /// Dictionary에 Class인 PopUp_Group 자체를 넣었음
+    /// </summary>
+    public void SetPopUpDict()
+    {
+        popUpDict = new Dictionary<string, PopUp_Group>();
+
+        foreach (var item in PopUp_List)
+        {
+            if (!popUpDict.ContainsKey(item.PopUp_Name))
+            {
+                popUpDict.Add(item.PopUp_Name, item);
+            }
+            else
+            {
+                Debug.LogWarning($"Duplicate PopUpName: {item.PopUp_Name}");
+            }
+        }
+    }
+
+    public void SetPanelDict()
+    {
+        panelDict = new Dictionary<string, Panel_Group>();
+
+        foreach (var item in Panel_List)
+        {
+            if (!panelDict.ContainsKey(item.Panel_Name))
+            {
+                panelDict.Add(item.Panel_Name, item);
+            }
+            else
+            {
+                Debug.LogWarning($"Duplicate PanelName: {item.Panel_Name}");
+            }
+        }
+    }
+    #endregion
+
+    #region 팝업 관련된 기능
+
+    /// <summary>
+    /// popUp_Name으로 지정한 팝업을 열기
+    /// </summary>
+    public void OpenPopUp(string popUp_Name)
+    {
+        if (popUpDict.TryGetValue(popUp_Name, out PopUp_Group popUp))
+        {
+            if (popUp.isAnim)
+            {
+                ShowUI(popUp.PopUp_Obj);
+            }
+            else
+            {
+                popUp.PopUp_Obj.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"No popup found with name: {popUp_Name}");
+        }
+    }
+
+    /// <summary>
+    /// popUp_Name으로 지정한 팝업을 닫기
+    /// </summary>
+    public void ClosePopUp(string popUp_Name)
+    {
+        if (popUpDict.TryGetValue(popUp_Name, out PopUp_Group popUp))
+        {
+            if (popUp.isAnim)
+            {
+                HideUI(popUp.PopUp_Obj);
+            }
+            else
+            {
+                popUp.PopUp_Obj.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"No popup found with name: {popUp_Name}");
+        }
+    } 
+
+    public void CloseAllPopUps()
+    {
+        foreach (var kvp in popUpDict)
+        {
+            ClosePopUp(kvp.Key);
+        }
+    }
+
+    #endregion
+}
