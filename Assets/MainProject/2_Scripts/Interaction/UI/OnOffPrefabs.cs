c431cf0ffa863f2ba9c8634f1c9aa73cd8884b14
LSeungHyun <noblesse991015@gmail.com>
2025-04-07 17:35:27 +0900

    튜토리얼 Obj PV 테스트

--- Assets/MainProject/2_Scripts/Interaction/UI/OnOffPrefabs.cs
+++ Assets/MainProject/2_Scripts/Interaction/UI/OnOffPrefabs.cs
@@ -1,11 +1,19 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class OnOffPrefabs : MonoBehaviour
 {
     public UIManager uIManager;
-
+    public PhotonView PV;
     void OnTriggerEnter2D()
     {
-        uIManager.OpenPopUp("Help_PopUp");
+        if (PV != null && PV.IsMine)
+        {
+            uIManager.OpenPopUp("Help_PopUp");
+        }
+        else
+        {
+            uIManager.OpenPopUp("Help_PopUp");
+        }
     }
 }
