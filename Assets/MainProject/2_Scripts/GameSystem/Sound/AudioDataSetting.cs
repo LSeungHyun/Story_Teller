b3d6e9657cc3dd1d031eb838867f0221f28af3e7
LSeungHyun <noblesse991015@gmail.com>
2025-04-08 11:04:16 +0900

    저승씬 audioSetting 빠져있던 기능들 연동 완료

--- Assets/MainProject/2_Scripts/GameSystem/Sound/AudioDataSetting.cs
+++ Assets/MainProject/2_Scripts/GameSystem/Sound/AudioDataSetting.cs
@@ -12,7 +12,27 @@ public class AudioDataSetting : MonoBehaviour
         bgmSlider.value = audioSettingsData.bgmVolume;
         sfxSlider.value = audioSettingsData.sfxVolume;
 
-        //Debug.Log(bgmSlider.value);
-        //Debug.Log(sfxSlider.value);
+
+        bgmSlider.onValueChanged.AddListener((volume) => {
+            if (SoundManager.Instance != null)
+            {
+                SoundManager.Instance.SetBGMVolume(volume);
+            }
+            else
+            {
+                Debug.LogWarning("SoundManager Instance가 존재하지 않습니다.");
+            }
+        });
+
+        sfxSlider.onValueChanged.AddListener((volume) => {
+            if (SoundManager.Instance != null)
+            {
+                SoundManager.Instance.SetSFXVolume(volume);
+            }
+            else
+            {
+                Debug.LogWarning("SoundManager Instance가 존재하지 않습니다.");
+            }
+        });
     }
 }
