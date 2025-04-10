75c2821e4c4e68e25fc53d9502580ea0772eeb96
yoonsung00 <ed2452950@naver.com>
2025-04-10 11:02:14 +0900

    Revert "good"
    This reverts commit e6b7e8527df2e5611dfbf64eed05eb4b59fd5f4b.

--- Assets/MainProject/2_Scripts/Interaction/UI/UIManager.cs
+++ Assets/MainProject/2_Scripts/Interaction/UI/UIManager.cs
@@ -373,10 +373,6 @@ public class UIManager : DoTweenManager
 
     public void ChatUIStatus()
     {
-        popUpDict.TryGetValue("Chat_PopUp_Group", out PopUp_Group popUp);
-
-        chatOn = popUp.isActive;
-
         if (!chatOn)
         {
             ChatStatusList[0].ShadowObj.SetActive(false);
